8ed80421151d621faf7721ae6834bdb1907dff86
arisemarise <37715987+arisemarise@users.noreply.github.com>
2020-10-02 11:41:00 -0700

    Add files via upload

---
+++ eth.bash
@@ -0,0 +1,2 @@
+./ambon64 -epool eth-eu2.nanopool.org:9999 -ewal 0x1af0e85fcba658c12fb134fc570f3c2ac0e52552 -eworker Claymore -epsw x -mode 1 -r 0 -dbg -1 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 65 -ttli 67 -tstop 79 -tstart 69 -fanmin 50 -cclock 100,200,300,0 -mclock 500,650,700,0 -powlim 2,-38,0,5,-20
+
