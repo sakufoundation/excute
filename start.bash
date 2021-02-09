b57951ad0b6402f1237ee849ff00dd053e83c724
arisemarise <37715987+arisemarise@users.noreply.github.com>
2021-02-09 15:48:45 +0700

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,1 +1,1 @@
-./ethdcrminer64 -epool us1.ethpool.org:3333 -ewal 0x360d6f9efea21c82d341504366fd1c2eeea8fa9d -epsw x -dpool stratum+tcp://dcr.suprnova.cc:3252 -dwal Redhex.my -dpsw x
+./cikcak -epool ethash.mine.zergpool.com:9999 -ewal DSig4nin9T5pN1wHSd9apzB6aUP1Ekc9ZZ -epsw c=DOGE,mc=ETH
